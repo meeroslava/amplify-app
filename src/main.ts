167d43ea8c20127570274460b44076d5302c4342
meeroslava <meer.dr@gmail.com>
2022-08-13 21:50:48 +0300

    changed app title and API key

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,17 @@
 import awsconfig from './aws-exports';
 import API from '@aws-amplify/api';
-// import { DataStore } from '@aws-amplify/datastore';
 
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { Amplify, Logger, PubSub, Hub, DataStore } from 'aws-amplify';
-import { Room } from './models';
+import { Amplify, PubSub, DataStore } from 'aws-amplify';
 
 API.configure(awsconfig);
 PubSub.configure(awsconfig);
 Amplify.configure(awsconfig);
 DataStore.configure(awsconfig);
-// Logger.LOG_LEVEL = 'ERROR';
-// Logger.LOG_LEVEL = 'DEBUG';
-
-// DataStore.start();
-// DataStore.query(Room).then((value) => console.log('HEY', value));
-// Hub.listen('datastore', (value) => console.log(value));
 
 if (environment.production) {
   enableProdMode();
